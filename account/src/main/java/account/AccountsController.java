05970b2508a2a1ece217ce7a5b51adb8618e4eac
QuangDương <minhquang.17820@gmail.com>
2023-09-07 01:25:06 +0700

    update

---
+++ account/src/main/java/account/AccountsController.java
@@ -0,0 +1,28 @@
+package account;
+
+import account.model.Accounts;
+import account.model.Customer;
+import account.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Eazy Bytes
+ */
+
+@RestController
+public class AccountsController {
+
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @PostMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+        return accountsRepository.findByCustomerId(customer.getCustomerId());
+
+    }
+
+}
