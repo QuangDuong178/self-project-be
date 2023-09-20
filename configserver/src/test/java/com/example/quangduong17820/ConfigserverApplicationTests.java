a06277bd1c097c1c213a41d652ecf27d054b586a
Duong Minh Quang <quangdm@vietis.com.vn>
2023-09-20 14:49:45 +0700

    update

---
+++ configserver/src/test/java/com/example/quangduong17820/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quangduong17820;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
